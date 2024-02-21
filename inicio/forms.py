a99eba7c6a4c25fc94c84af340e9f95cd2259dc6
alfredo strubia <strubia.alfredo@gmail.com>
2024-02-21 11:48:27 -0300

    correccion de entrega3

--- inicio/forms.py
+++ inicio/forms.py
@@ -4,4 +4,4 @@ class FormularioAltaPaciente(forms.Form):
     nombre = forms.CharField(max_length=20)
     apellido = forms.CharField(max_length=20)
     edad = forms.IntegerField()
-    insulinoterapia= forms.BooleanField(default=False)
+    insulinoterapia= forms.BooleanField()
